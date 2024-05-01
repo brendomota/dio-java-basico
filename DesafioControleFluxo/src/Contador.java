3cc0f25b7adbba1c124896da52f672ce0756008f
brendomota <brendo.saescola@gmail.com>
2024-05-01 16:08:09 -0300

    renomeando o arquivo principal para Contador.java, criando a classe ParametrosInvalidosException e desenvolvendo o código final

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+
+        Scanner terminal = new Scanner(System.in);
+
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+        terminal.nextLine();
+
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+        terminal.nextLine();
+		
+		try {
+			//chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException exception) {
+			System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		if(parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+		int contagem = parametroDois - parametroUm;
+		for(int i = 1; i <= contagem; i++) {
+            System.out.println("Impriminto o número " + i);
+        }
+	}
+}
---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+        super("Os parâmetros fornecidos são inválidos.");
+    }
+}
